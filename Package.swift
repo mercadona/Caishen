5612b40b418584a5b2e9f2a87cb9adbed01aa4f6
Miguel Ibañez Paricio <mibanepa@mercadona.es>
2020-10-13 16:00:49 +0200

    Update to swift 5, create Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Presentr",
+    platforms: [.iOS(.v11)],
+    products: [.library(name: "Caishen", targets: ["Caishen"])],
+    dependencies: [],
+    targets: [.target(name: "Caishen", path: "Pod/Classes")]
+)
